233dd1e8b34ff60aaa50b4ddc4e886c7e6e0eedb
totsky.ia <valihmanov777@gmail.com>
2023-11-20 17:05:46 +0300

    Добавлено решение 27го задания

--- src/index.js
+++ src/index.js
@@ -5,11 +5,39 @@ import calc from "./modules/calc";
 import through from "./modules/through";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
+import sendForm from "./modules/sendForm";
 
-timer("31 october 2023");
+timer("30 november 2023");
 menu();
 modal();
 calc(100);
 through();
 tabs();
 slider();
+sendForm({
+  formId: "form1",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
+sendForm({
+  formId: "form2",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
+sendForm({
+  formId: "form3",
+  someElem: [
+    {
+      type: "block",
+      id: "total",
+    },
+  ],
+});
